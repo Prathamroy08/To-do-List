bb9a8b68857d742d345bc81c1dd2efad4ba0a2f1
Prathamroy08 <prathamroy08@gmail.com>
2025-09-24 19:33:57 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { useState } from "react";
+
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+} from "react-native";
 
 export default function App() {
+
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    // console.log(enteredText);
+    setEnteredGoalText(enteredText);
+  }
+  function addGoalHandler() {
+    // console.log(enteredGoalText);
+    setCourseGoals(currentCourseGoals => [
+      ...currentCourseGoals,
+      enteredGoalText,
+
+    ]);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.title}>GOAL SETTER</Text>
+      </View>
+
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal"
+          placeholderTextColor="#888"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
+      </View>
+
+      <View style={styles.listContainer}>
+        {/* <Text style={styles.listTitle}>List of Goals...</Text> */}
+        {courseGoals.map((goal, index) => (
+          <Text style={styles.listTitle} key={index}>{goal}</Text>
+        ))}
+      </View>
     </View>
   );
 }
@@ -13,8 +55,44 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 60,
+    paddingHorizontal: 20,
+    backgroundColor: "#f9f9f9",
+  },
+  header: {
+    paddingBottom: 20,
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: "bold",
+    color: "#673030",
+    backgroundColor:'#f2f3f4',
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+    paddingBottom: 12,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#673030",
+    width: "75%",
+    padding: 10,
+    borderRadius: 6,
+    fontSize: 16,
+    backgroundColor: "#fff",
+  },
+  listContainer: {
+    marginTop: 20,
+  },
+  listTitle: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: "#333",
   },
 });
